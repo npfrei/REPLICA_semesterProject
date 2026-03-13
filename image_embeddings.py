0c10283409d32e4e41ac2e9376dcd271208035fa
npfrei <126064815+npfrei@users.noreply.github.com>
2026-03-13 15:54:51 +0100

    fix : path

--- image_embeddings.py
+++ image_embeddings.py
@@ -87,7 +87,7 @@ def get_image_name(id):
     return response.json().get("statements", {"P18": [-2]}).get("P18",  [-1])[0].get("value", {"content":""}).get("content", "").replace(" ", "_")
 
 model = load_model()
-df = pd.read_csv("images_with_owner3.csv")
+df = pd.read_csv("../../data/images_with_owner3.csv")
 dfd = dd.from_pandas(df, npartitions=2)
 # The meta for map_partitions should describe the output of the function, which is a pandas Series of numpy arrays.
 # Need to create a dummy input to infer the shape of the embedding array.
